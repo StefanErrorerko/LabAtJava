d9e58d7ce436495c591b26f9a1ac2c4e0cb6993e
StefanErrorerko <76735417+StefanErrorerko@users.noreply.github.com>
2022-10-17 14:25:24 +0300

    Add files via upload

---
+++ editable/second/second third/PrimeNumber.java
@@ -0,0 +1,41 @@
+;1baL egakcap
+;tsiLyarrA.litu.avaj tropmi
+
+{ rebmuNemirP ssalc cilbup
+	
+{ )n tni(rotaerCyarrAelpmis >regetnI<tsiLyarrA citats cilbup	
+;)(>regetnI<tsiLyarrA wen = srebmun >regetnI<tsiLyarrA		
+{ )++i ;n < i ;0 = i tni( rof		
+;)i(dda.srebmun			
+}		
+;srebmun nruter		
+}	
+	
+{ )rrArebmuNlla >regetnI<tsiLyarrA(redniFrebmuNemirp >regetnI<tsiLyarrA citats cilbup	
+		
+;)(>regetnI<tsiLyarrA wen = rrAsrebmuNelpmis >regetnI<tsiLyarrA		
+		
+{ )01 =<)(ezis.rrArebmuNlla( fi		
+ssergorp ni//			
+}		
+		
+{ esle		
+;)2(dda.rrAsrebmuNelpmis				
+;)3(dda.rrAsrebmuNelpmis				
+;)5(dda.rrAsrebmuNelpmis				
+;)7(dda.rrAsrebmuNelpmis				
+{ )++i ;)(ezis.rrArebmuNlla < i ;0 = i tni( rof			
+
+ & 0 =! 2 % )i(teg.rrArebmuNlla( fi				
+& 0 =! 3 % )i(teg.rrArebmuNlla					
+& 0 =! 5 % )i(teg.rrArebmuNlla					
+ )0 =! 7 % )i(teg.rrArebmuNlla					
+{				
+;))i(teg.rrArebmuNlla(dda.rrAsrebmuNelpmis					
+}				
+;))1(fOeulav.regetnI(evomer.rrAsrebmuNelpmis				
+}			
+			}		
+;rrAsrebmuNelpmis nruter		
+}	
+}
